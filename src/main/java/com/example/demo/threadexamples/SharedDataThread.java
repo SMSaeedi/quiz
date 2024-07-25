522e9eff4ba4c711603a3e8280d4bf0ae3db7c1d
SMSaeedi <mahsa.saeedy@gmail.com>
2024-07-25 15:08:55 +0200

    My self-practice codes

--- src/main/java/com/example/demo/threadexamples/SharedDataThread.java
+++ src/main/java/com/example/demo/threadexamples/SharedDataThread.java
@@ -1,46 +1,126 @@
 package com.example.demo.threadexamples;
 
-public class SharedDataThread implements Runnable {
-    private final SharedData sharedData;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.SneakyThrows;
 
-    public SharedDataThread(SharedData sharedData) {
-        this.sharedData = sharedData;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Getter
+@Setter
+class SimpleIntSharedData {
+    public int data;
+
+    public SimpleIntSharedData(int sharedData) {
+        this.data = sharedData;
     }
 
-    @Override
-    public void run() {
-        int data = sharedData.getData();
-        sharedData.setData(data + 2);
-    }
+//    public void modifyingSharedData(int data) {
+//        setData(data + 125);
+//    }
 
+    @SneakyThrows
     public static void main(String[] args) {
-        SharedData data = new SharedData();
-        data.setData(244);
+        SimpleIntSharedData simpleIntSharedData = new SimpleIntSharedData(100);
 
-        Thread thread1 = new Thread(new SharedDataThread(data));
-        Thread thread2 = new Thread(new SharedDataThread(data));
+        Runnable task1 = () -> {
+            simpleIntSharedData.setData(25);
+            System.out.println("1st thread --> " + simpleIntSharedData.getData());
+        };
+
+        Runnable task2 = () -> {
+            simpleIntSharedData.setData(35);
+            System.out.println("2nd thread --> " + simpleIntSharedData.getData());
+        };
+
+        Runnable task3 = () -> {
+            simpleIntSharedData.setData(150);
+            System.out.println("3nd thread --> " + simpleIntSharedData.getData());
+        };
+
+        Thread thread1 = new Thread(task1);
+        Thread thread2 = new Thread(task2);
+        Thread thread3 = new Thread(task3);
 
         thread1.start();
-        thread2.start();
+        thread1.join(); //wait the thread/s until thread1 to finish
 
-        try {
-            thread1.join();
-            thread2.join();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        System.out.println("final value: " + data.getData());
+        thread2.start();
+        thread2.join();
+
+        thread3.start();
+        thread3.join();
+
+        System.out.println("Shared data final value --> " + simpleIntSharedData.getData());
     }
 }
 
-class SharedData {
-    private int data;
+class AtomicSharedData {
+    public AtomicInteger sharedData;
 
-    public int getData() {
-        return data;
+    public AtomicSharedData(int value) {
+        sharedData = new AtomicInteger(value);
     }
 
-    public void setData(int data) {
-        this.data = data;
+    public static void main(String[] args) {
+        AtomicSharedData useSharedData = new AtomicSharedData(100);
+
+        Runnable task1 = () -> {
+            useSharedData.sharedData.addAndGet(25);
+            System.out.println("1st thread --> " + useSharedData.sharedData.get());
+        };
+
+        Runnable task2 = () -> {
+            useSharedData.sharedData.addAndGet(35);
+            System.out.println("2nd thread --> " + useSharedData.sharedData.get());
+        };
+
+        Runnable task3 = () -> {
+            useSharedData.sharedData.addAndGet(150);
+            System.out.println("3rd thread --> " + useSharedData.sharedData.get());
+        };
+
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+        executor.submit(task3);
+        executor.submit(task1);
+        executor.submit(task2);
+
+        executor.shutdown();
+    }
+}
+
+class ConcurrentHashMapSharedData {
+    public ConcurrentHashMap<String, Integer> concurrentMap;
+
+    public ConcurrentHashMapSharedData(int value) {
+        concurrentMap = new ConcurrentHashMap<>();
+        concurrentMap.put("data", value);
+    }
+
+    public void modifyingSharedData(int data) {
+        concurrentMap.merge("data", data, Integer::sum);
+    }
+
+    public static void main(String[] args) {
+        ConcurrentHashMapSharedData useSharedData = new ConcurrentHashMapSharedData(100);
+
+        Runnable task1 = () -> {
+            useSharedData.modifyingSharedData(25);
+            System.out.println("1st thread --> " + useSharedData.concurrentMap.get("data"));
+        };
+
+        Runnable task2 = () -> {
+            useSharedData.modifyingSharedData(35);
+            System.out.println("2nd thread --> " + useSharedData.concurrentMap.get("data"));
+        };
+
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        executor.submit(task1);
+        executor.submit(task2);
+
+        executor.shutdown();
     }
 }
--- src/main/java/com/example/demo/threadexamples/ThreadLocalSharedData.java
+++ src/main/java/com/example/demo/threadexamples/ThreadLocalSharedData.java
@@ -3,7 +3,7 @@ package com.example.demo.threadexamples;
 public class ThreadLocalSharedData {
     /**
      * Local threads are local variables in which each thread can access them through getter/setter,
-     * and independently initialized copy of the variable in each thread.
+     * and each thread independently initialized copy of the variable.
      */
     private static final ThreadLocal<Integer> threadLocal = new ThreadLocal<>();
 
@@ -18,10 +18,17 @@ public class ThreadLocalSharedData {
             System.out.println("Thread 2 --> " + threadLocal.get());
         };
 
+        Runnable task3 = () -> {
+            threadLocal.set(250);
+            System.out.println("thread 3 --> " + threadLocal.get());
+        };
+
         Thread thread1 = new Thread(task1);
         Thread thread2 = new Thread(task2);
+        Thread thread3 = new Thread(task3);
 
         thread1.start();
         thread2.start();
+        thread3.start();
     }
 }
