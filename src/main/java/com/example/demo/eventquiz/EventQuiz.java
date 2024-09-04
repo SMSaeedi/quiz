1886b85ecd1b277fe5ab83a043c13259d29cc10e
SMSaeedi <mahsa.saeedy@gmail.com>
2024-09-04 15:47:00 +0200

    new example code (socket programming)

--- src/main/java/com/example/demo/eventquiz/EventQuiz.java
+++ src/main/java/com/example/demo/eventquiz/EventQuiz.java
@@ -4,16 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EventQuiz {
-    private static List<NewEvent> events = new ArrayList<>();
+    private static List<Event> events = new ArrayList<>();
 
     public static void main(String[] args) {
-        events.add(new NewEvent("foo", 1000000L));
-        events.add(new NewEvent("bar", 1000003L));
-        events.add(new NewEvent("baz", 1000006L));
-        events.add(new NewEvent("foo", 1000009L));
-        events.add(new NewEvent("bar", 1000013L));
-        events.add(new NewEvent("baz", 1000016L));
-        events.add(new NewEvent("foo", 1000013L));
+        events.add(new Event("foo", 1000000L));
+        events.add(new Event("bar", 1000003L));
+        events.add(new Event("baz", 1000006L));
+        events.add(new Event("foo", 1000009L));
+        events.add(new Event("bar", 1000013L));
+        events.add(new Event("baz", 1000016L));
+        events.add(new Event("foo", 1000013L));
 
         List<String> result = new ArrayList<>();
         for (int i = 0; i < events.size(); i++)
@@ -29,11 +29,11 @@ public class EventQuiz {
     }
 }
 
-class NewEvent {
+class Event {
     private String name;
     private Long timestamp;
 
-    public NewEvent(String name, Long timestamp) {
+    public Event(String name, Long timestamp) {
         this.name = name;
         this.timestamp = timestamp;
     }
