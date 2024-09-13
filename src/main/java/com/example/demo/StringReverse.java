a881a1ab06d2faed706047ecdd56c7aba9da1445
SMSaeedi <mahsa.saeedy@gmail.com>
2024-09-13 16:40:38 +0200

    added some missing classes

--- src/main/java/com/example/demo/StringReverse.java
+++ src/main/java/com/example/demo/StringReverse.java
@@ -5,7 +5,7 @@ public class StringReverse {
         String str = "Reverse Me";
         StringBuffer sb = new StringBuffer();
         for (int i = str.length() - 1; i >= 0; i--)
-            sb = sb.append(str.charAt(i));
+            sb.append(str.charAt(i));
         System.out.println(sb);
 
         String str2 = "You Reversed Me";
