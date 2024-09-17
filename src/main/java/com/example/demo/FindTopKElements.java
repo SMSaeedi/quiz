0d2a1b7f13a57df05d053f577910a7d75b47336b
SMSaeedi <mahsa.saeedy@gmail.com>
2024-09-17 11:47:12 +0200

    added findMax using B-Search

--- src/main/java/com/example/demo/FindTopKElements.java
+++ src/main/java/com/example/demo/FindTopKElements.java
@@ -31,3 +31,30 @@ class FindSecondTopElement {
                 });
     }
 }
+
+class FindMaxUsingBinarySearch{
+    public static void main(String[] args) {
+        int[] nums = new int[] {4, 3, 2, 1, 7, 6, 5};
+        System.out.println(findMax(nums));  // Output: 7
+
+        int[] nums2 = new int[] {3, 2, 1};
+        System.out.println(findMax(nums2));  // Output: 3
+
+        int[] nums3 = new int[] {6, 5, 4, 3, 2, 1, 7};
+        System.out.println(findMax(nums3));  // Output: 7
+    }
+
+    public static int findMax(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] > nums[mid + 1])
+                high = mid;
+            else
+                low = mid + 1;
+        }
+        return nums[low];
+    }
+}
