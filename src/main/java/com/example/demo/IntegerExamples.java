07c6df858f5c882e3a22feadb3890e41f270d268
SMSaeedi <mahsa.saeedy@gmail.com>
2024-10-02 11:50:25 +0200

    Added FizzBuzz

--- src/main/java/com/example/demo/IntegerExamples.java
+++ src/main/java/com/example/demo/IntegerExamples.java
@@ -182,3 +182,18 @@ class AverageTwoArrays {
         System.out.println("Average " + average);
     }
 }
+
+class FizzBuzz {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 15; i++) {
+            if (i % 3 == 0 && i % 5 == 0)
+                System.out.println("FizzBuzz");
+            else if (i % 3 == 0)
+                System.out.println("Fizz");
+            else if (i % 5 == 0)
+                System.out.println("Buzz");
+            else
+                System.out.println(i);
+        }
+    }
+}
