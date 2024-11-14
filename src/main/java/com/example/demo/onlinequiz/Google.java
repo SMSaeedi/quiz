384df35309ab6f0b30fbfa419216aca4ca3b3dcc
SMSaeedi <mahsa.saeedy@gmail.com>
2024-11-14 15:46:01 +0100

    some optimizations

--- src/main/java/com/example/demo/onlinequiz/Google.java
+++ src/main/java/com/example/demo/onlinequiz/Google.java
@@ -6,7 +6,7 @@ public class Google {
     public static void main(String[] args) {
         String str = "bad";
         int result = 0;
-        for (int i = 0; i < str.length() - 1; i++)
+        for (int i = 0; i < str.length() - 1; i++) // length - 1, for the last char no need to iterate
             result += Math.abs(str.charAt(i) - str.charAt(i + 1));
 
         out.println(str + " " + result);
