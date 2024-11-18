ad2ab88c48c8f01549ef42c711a245d398a08d40
SMSaeedi <mahsa.saeedy@gmail.com>
2024-11-18 16:54:40 +0100

    some optimizations

--- src/main/java/com/example/demo/onlinequiz/Google.java
+++ src/main/java/com/example/demo/onlinequiz/Google.java
@@ -6,12 +6,21 @@ public class Google {
     public static void main(String[] args) {
         String str = "bad";
         findCharDistance(str);
+        findCharDistanceUsingToCharArray(str);
     }
 
     private static void findCharDistance(String str) {
         int result = 0;
         for (int i = 0; i < str.length() - 1; i++) // length - 1, for the last char no need to iterate
             result += Math.abs(str.charAt(i) - str.charAt(i + 1));
+
+        out.println(str + " " + result);
+    }
+
+    private static void findCharDistanceUsingToCharArray(String str) {
+        int result = 0;
+        for (int i = 0; i < str.toCharArray().length - 1; i++)
+            result += Math.abs(str.toCharArray()[i] - str.toCharArray()[i + 1]);
 
         out.println(str + " " + result);
     }
