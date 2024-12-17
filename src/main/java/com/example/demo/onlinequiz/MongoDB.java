cad69684fcf2eea85a321460c5a7310abd7c3d81
SMSaeedi <mahsa.saeedy@gmail.com>
2024-12-17 16:52:16 +0100

    some improvements

--- src/main/java/com/example/demo/onlinequiz/MongoDB.java
+++ src/main/java/com/example/demo/onlinequiz/MongoDB.java
@@ -1,5 +1,8 @@
 package com.example.demo.onlinequiz;
 
+import lombok.AllArgsConstructor;
+import lombok.experimental.Accessors;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,14 +12,30 @@ class MongoDB {
         int num = 10;
 
         modifyValues(str, num);
-
         System.out.println("String after modification: " + str); //Hello
         System.out.println("Integer after modification: " + num); //10
+
+        MyObj myObj = modifyValues1(str, num);
+        System.out.println("String after modification: " + myObj.str); //World
+        System.out.println("Integer after modification: " + myObj.num); //20
     }
 
     public static void modifyValues(String str, int num) {
         str = "World";
         num = 20;
+    }
+
+    public static MyObj modifyValues1(String str, int num) {
+        str = "World";
+        num = 20;
+        return new MyObj(str, num);
+    }
+
+    @AllArgsConstructor
+    @Accessors(fluent = true)
+    static class MyObj {
+        String str;
+        int num;
     }
 }
 
@@ -31,10 +50,19 @@ class ClassPerson {
         person = new ClassPerson("Jon");
     }
 
+    public static ClassPerson modifyObject2(ClassPerson person) {
+        person = new ClassPerson("Jon");
+        return person;
+    }
+
     public static void main(String[] args) {
         ClassPerson person = new ClassPerson("Jane");
         modifyObject(person);
         System.out.println(person.name); // What will be printed here? Jane
+
+        ClassPerson person2 = new ClassPerson("Jane");
+        person2 = modifyObject2(person2);
+        System.out.println(person2.name); // What will be printed here? Jon
     }
 }
 
@@ -68,7 +96,6 @@ class Circle extends Shape {
         int r = 5;
         return 3.14 * r * r;
     }
-
 }
 
 class Rectangle extends Shape {
@@ -92,18 +119,18 @@ class Rectangle extends Shape {
 /**
  * DB question
  * Given the following table structure:
- *      Person:
- *          - id
- *          - first_name
- *          - last_name
- *      Person_Address:
- *          - person_id
- *          - address_id
- *      Address:
- *          - id
- *          - street
- *          - city
- *          - zipcode
+ * Person:
+ * - id
+ * - first_name
+ * - last_name
+ * Person_Address:
+ * - person_id
+ * - address_id
+ * Address:
+ * - id
+ * - street
+ * - city
+ * - zipcode
  * Write a SQL query that lists how many people live on each address?
  * Example output:
  * address_id | street               |  no_tenants
@@ -112,16 +139,8 @@ class Rectangle extends Shape {
  * b          |  Long Lane 1023      |  17
  * c          |  Queens Road 7       |  1
  * Query:
- * SELECT
- *     pa.address_id,
- *     a.street,
- *     COUNT(pa.person_id) AS no_tenants
- * FROM
- *     Person_Address pa
- * JOIN
- *     Address a ON pa.address_id = a.id
- * GROUP BY
- *     pa.address_id, a.street
- * ORDER BY
- *     pa.address_id;
+ * SELECT pa.address_id, a.street, COUNT(pa.person_id) AS no_tenants
+ * FROM Person_Address pa JOIN Address a ON pa.address_id = a.id
+ * GROUP BY pa.address_id, a.street
+ * ORDER BY pa.address_id;
  */
